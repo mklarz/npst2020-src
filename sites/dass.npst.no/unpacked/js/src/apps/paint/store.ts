4be4b529b454348ce1ca12419946bf058be28da5
mklarz <mklarz@protonmail.com>
2020-12-10 17:20:04 +0000

    [npst.no] UPDATE

--- sites/dass.npst.no/unpacked/js/src/apps/paint/store.ts
+++
@@ -1,9 +0,0 @@
-import { writable } from "svelte/store";
-import colors from "./colors";
-export default writable({
-    canvasSize: [250, 200],
-    primaryColor: colors[0],
-    secondaryColor: colors[1],
-    currentTool: "pencil",
-    brushSize: 2,
-});
